7ed8ec1b07ac863e07d3700e7445b276df18a927
Vrash <vrashabh86@gmail.com>
2023-12-13 20:25:32 +0000

    Day 13 refactor for clarity

--- 2023/13.py
+++ 2023/13.py
@@ -7,12 +7,20 @@ filename = "/Users/.../Desktop/Advent-of-Code/2023/input.txt"
 grid = [[x for x in s.split('\n')if x]for s in open(filename).read().split('\n\n')]
 parts = defaultdict(int)
 for entry in grid:
+     #loop across grid and transpose of grid, using multiplier tuples
     for multiplier, pattern in [(100, entry), (1, list(zip(*entry)))]:
         for i in range(1, len(pattern)):
-            p1 = pattern[i:]
-            p2 = pattern[:i][::-1]
-            differences = sum(c1 != c2 for l1, l2 in zip(p1, p2) for c1, c2 in zip(l1, l2))
+            #next line
+            below_or_right = pattern[i:]
+            
+            #previous line reversed
+            above_or_left = pattern[:i][::-1]
+            
+            #find how many characters are different between the two and add to dict
+            differences = sum(c1 != c2 for l1, l2 in zip(below_or_right, above_or_left) for c1, c2 in zip(l1, l2))
+            
+            #build dictionary of differences with rules multiplier i.e. 100 for horizontal and 1 for vertical (transposed)
             parts[differences] += multiplier * i
 
-print(parts[0]) #no smudge
-print(parts[1]) #1 smudge
+print(parts[0]) #no smudge i.e. no differences found
+print(parts[1]) #1 smudge i.e. for 1 difference
